dbacc58d7d22a1cba79cdd18512b0acf2d89ca73
YUU OTOSAKA <111130180+WellingtonGabriel@users.noreply.github.com>
2022-10-28 11:11:52 -0300

    first

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
+import Contador from "./components/contador"; 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div>
+      <Contador/>
     </div>
   );
 }
-
 export default App;
